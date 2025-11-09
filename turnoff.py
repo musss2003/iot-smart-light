0bf760b93d53c50a6ea749d6b6fa46e3171e6ffb
MrR1ad <riad.elezovic@outlook.com>
2025-11-09 15:54:38 +0100

    Updated turnoff

--- turnoff.py
+++ turnoff.py
@@ -1,6 +1,19 @@
+# /var/www/html/turnoff.py
 import lgpio
-
+import os
+import signal
+ 
 LED_PIN = 17
+PID_FILE = "/tmp/blink_led.pid"
+ 
+# Kill blinking if running
+if os.path.exists(PID_FILE):
+    with open(PID_FILE, "r") as f:
+        pid = int(f.read())
+    os.kill(pid, signal.SIGTERM)
+    os.remove(PID_FILE)
+ 
+# Turn LED OFF
 h = lgpio.gpiochip_open(0)
 lgpio.gpio_claim_output(h, LED_PIN)
 lgpio.gpio_write(h, LED_PIN, 0)
