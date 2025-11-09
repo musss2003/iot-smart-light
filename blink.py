4357cdb9a3ddd2b5e20c16b0439f580672959c9c
MrR1ad <riad.elezovic@outlook.com>
2025-11-09 15:54:26 +0100

    Updated blink

--- blink.py
+++ blink.py
@@ -1,15 +1,25 @@
+# /var/www/html/blink.py
 import lgpio
 import time
-
+import os
+ 
 LED_PIN = 17
+PID_FILE = "/tmp/blink_led.pid"
+ 
+# Save PID of this process
+with open(PID_FILE, "w") as f:
+    f.write(str(os.getpid()))
+ 
 h = lgpio.gpiochip_open(0)
 lgpio.gpio_claim_output(h, LED_PIN)
-
+ 
 try:
     while True:
         lgpio.gpio_write(h, LED_PIN, 1)
         time.sleep(0.5)
         lgpio.gpio_write(h, LED_PIN, 0)
         time.sleep(0.5)
-except KeyboardInterrupt:
+finally:
     lgpio.gpiochip_close(h)
+    if os.path.exists(PID_FILE):
+        os.remove(PID_FILE)
